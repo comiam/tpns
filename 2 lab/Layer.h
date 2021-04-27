8e14d195398b5502e7ab90c3927c82794da16bdb
Comiam <maxim.bolshim@yandex.ru>
2021-04-27 11:52:38 +0700

    add 2 lab

--- 2 lab/Layer.h
+++ 2 lab/Layer.h
@@ -1,8 +1,60 @@
-//
-// Created by Admin on 20.04.2021.
-//
+#ifndef PERCEPTRON_LAYER_H
+#define PERCEPTRON_LAYER_H
 
-#ifndef INC_2_LAB_LAYER_H
-#define INC_2_LAB_LAYER_H
+#include "./NumCpp.hpp"
+#include "util.h"
 
-#endif //INC_2_LAB_LAYER_H
+using NdArrayF = nc::NdArray<float>;
+
+class Layer
+{
+private:
+    NdArrayF layerOutput;
+    NdArrayF layerWeights;
+
+public:
+    Layer(unsigned layerSize, unsigned prevLayerSize)
+    {
+        layerOutput = nc::zeros<float>({1, layerSize});
+        layerWeights = nc::zeros<float>({prevLayerSize, layerSize});
+    };
+
+    void initWeights(float low, float high)
+    {
+        layerWeights = nc::random::uniform<float>(nc::shape(layerWeights), low, high);
+    }
+
+    void activateNeurones(const NdArrayF &input)
+    {
+        layerOutput = nc::matmul(input, layerWeights);
+        auto outputSize = layerOutput.size();
+        for (auto i = 0; i < outputSize; ++i)
+        {
+            float neuroneActivation = sigmoid(layerOutput[i]);
+            layerOutput[i] = neuroneActivation;
+        }
+    }
+
+    const NdArrayF &getLayerOutput() const
+    {
+        return layerOutput;
+    }
+
+    void setLayerOutput(const NdArrayF &layerOutput)
+    {
+        Layer::layerOutput = layerOutput;
+    }
+
+    const NdArrayF &getLayerWeights() const
+    {
+        return layerWeights;
+    }
+
+    void setLayerWeights(const NdArrayF &layerWeights)
+    {
+        Layer::layerWeights = layerWeights;
+    }
+};
+
+
+#endif
